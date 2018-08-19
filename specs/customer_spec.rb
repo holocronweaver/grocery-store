6fc4a098d9cd2b12f0b68d077cd5363fc31a9670
Dan Roberts <dan@adadevelopersacademy.org>
2018-08-19 10:55:01 -0700

    Add customer specs

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -2,40 +2,76 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
-# TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/customer'
+require_relative '../lib/customer'
 
-xdescribe "Customer" do
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+describe "Customer Wave 1" do
+  ID = 123
+  EMAIL = "a@a.co"
+  ADDRESS = {
+    street: "123 Main",
+    city: "Seattle",
+    state: "WA",
+    zip: "98101"
+  }.freeze
+
   describe "#initialize" do
     it "Takes an ID, email and address info" do
-      # TODO: Your test code here!
+      cust = Customer.new(ID, EMAIL, ADDRESS)
+
+      cust.must_respond_to :id
+      cust.id.must_equal ID
+
+      cust.must_respond_to :email
+      cust.email.must_equal EMAIL
+
+      cust.must_respond_to :address
+      cust.address.must_equal ADDRESS
     end
   end
+end
 
+# TODO: remove the 'x' in front of this block when you start wave 2
+xdescribe "Customer Wave 2" do
   describe "Customer.all" do
     it "Returns an array of all customers" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-      #   - Customer.all returns an array
-      #   - Everything in the array is a Customer
-      #   - The number of orders is correct
-      #   - The ID, email address of the first and last
-      #       customer match what's in the CSV file
-      # Feel free to split this into multiple tests if needed
+      customers = Customer.all
+
+      customers.length.must_equal 35
+      customers.each do |c|
+        c.must_be_kind_of Customer
+      end
+    end
+
+    it "Returns accurate information about the first customer" do
+      first = Customer.all.first
+      first.id.must_equal 1
+    end
+
+    it "Returns accurate information about the last customer" do
+      last = Customer.all.last
+      last.id.must_equal 35
     end
   end
 
   describe "Customer.find" do
     it "Can find the first customer from the CSV" do
-      # TODO: Your test code here!
+      first = Customer.find(1)
+
+      first.must_be_kind_of Customer
+      first.id.must_equal 1
     end
 
     it "Can find the last customer from the CSV" do
-      # TODO: Your test code here!
+      last = Customer.find(35)
+
+      last.must_be_kind_of Customer
+      last.id.must_equal 35
     end
 
-    it "Raises an error for a customer that doesn't exist" do
-      # TODO: Your test code here!
+    it "Returns nil for a customer that doesn't exist" do
+      expect(Customer.find(53145)).must_be_nil
     end
   end
 end
